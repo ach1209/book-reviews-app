4efe439764f15aa8214b5f09b0fb6bc90b6cfd5e
Anthony <ach0074@gmail.com>
2022-01-25 15:44:21 -0500

    Placeholder styling for App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,13 @@
 </script>
 
 <template>
-  <div></div>
+  <div class="max-w-screen-xl min-h-screen mx-auto">
+    <h1>Hello World</h1>
+  </div>
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  @apply bg-slate-200;
 }
 </style>
