8e7d63986f9920c87087abd8a09c247a5b8a78b3
Anthony <ach0074@gmail.com>
2022-01-27 13:19:20 -0500

    Added search to home template

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,25 @@
+<script setup>
+import { ref } from 'vue'
+
+const iValue = ref('')
+
+// preliminary for now. fix later
+async function getSearchResults() {
+  if (iValue.value) {
+    const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=intitle:${iValue.value}`)
+    const results = await response.json()
+    console.log(results)
+    iValue.value = ''
+  } else {
+    return null
+  }
+}
+</script>
+
 <template>
-  <div>Home template</div>
+  <label for="search" class="font-inter text-sm">Please enter a title</label>
+  <input
+    type="text" v-model="iValue" id="search"
+    @keyup.native.enter="getSearchResults"
+    class="w-full h-12 px-2 rounded border border-indigo-500 bg-indigo-50 font-inter font-semibold focus:outline-none" />
 </template>
