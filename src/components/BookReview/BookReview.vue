25f6d339d269485fbf3884d861b4a46a9e84a639
Anthony <ach0074@gmail.com>
2022-02-17 14:54:38 -0500

    Changed text and added styling to BookReview component

--- src/components/BookReview/BookReview.vue
+++ src/components/BookReview/BookReview.vue
@@ -1,3 +1,7 @@
 <template>
-  <p>list of reviews here</p>
+  <div class="mt-2">
+    <div class="h-16 bg-blue-100 rounded-md flex items-center justify-center">
+      <p class="text-center text-base">There are currently no reviews available for this book.</p>
+    </div>
+  </div>
 </template>
