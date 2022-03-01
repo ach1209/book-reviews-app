f334156dd1e0448fe132768cc037c25979174d70
Anthony <ach0074@gmail.com>
2022-03-01 11:13:56 -0500

    Minor change to styles

--- src/components/BookReview/BookReview.vue
+++ src/components/BookReview/BookReview.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="mt-2">
+  <div class="mt-4">
     <div class="h-16 bg-blue-100 rounded-md flex items-center justify-center">
       <p class="text-center text-base">There are currently no reviews available for this book.</p>
     </div>
