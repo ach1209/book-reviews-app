5af9108bd9aeaddc7ee397d504b9d96ac2545f18
Anthony <ach0074@gmail.com>
2022-03-01 14:10:04 -0500

    Created ReviewForm component with preliminary code

--- src/components/BookReview/BookReview.vue
+++ src/components/BookReview/BookReview.vue
@@ -1,7 +1,30 @@
+<script setup>
+import { ref } from 'vue'
+import ReviewForm from '../ReviewForm/ReviewForm.vue'
+
+const displayForm = ref(false)
+
+function toggleForm() {
+  displayForm.value = !displayForm.value
+}
+</script>
+
 <template>
   <div class="mt-4">
-    <div class="h-16 bg-blue-100 rounded-md flex items-center justify-center">
+    <div class="min-h-max py-4 bg-blue-100 rounded-md flex flex-col items-center justify-center">
       <p class="text-center text-base">There are currently no reviews available for this book.</p>
+      <p class="text-center text-base leading-9">Be the first to
+        <span 
+          @click="toggleForm"
+          class="text-blue-600 hover:text-blue-700 cursor-pointer"
+        >write</span> a review!
+      </p>
+    </div>
+    <div 
+      v-if="displayForm"
+      class="mt-4"
+    >
+      <ReviewForm />
     </div>
   </div>
 </template>
---
+++ src/components/ReviewForm/ReviewForm.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { ref } from 'vue'
+import AppButton from '../AppButton/AppButton.vue'
+
+const isRecommended = ref('')
+const userReview = ref('')
+const reviewsList = []
+
+function formSubmitTest() {
+  reviewsList.push(
+    {
+      recommended: isRecommended.value,
+      userReview: userReview.value
+    }
+  )
+  resetForm()
+  console.log(reviewsList)
+}
+
+function resetForm() {
+  isRecommended.value = ''
+  userReview.value = ''
+}
+
+</script>
+
+<template>
+  <form @submit.prevent="formSubmitTest">
+    <div class="mb-4">
+      <p class="text-sm">Would you recommend this book?</p>
+      <input type="radio" id="yes" value="Yes" v-model="isRecommended">
+      <label for="yes" class="text-sm mx-2">Yes</label>
+      <input type="radio" id="no" value="No" v-model="isRecommended">
+      <label for="no" class="text-sm mx-2">No</label>      
+    </div>
+    <textarea 
+      name="review-section" id="review-section" cols="30" rows="10"
+      class="w-full p-2 border text-sm focus:outline-none"
+      v-model="userReview"
+    ></textarea>
+    <div class="mt-4 flex justify-end">
+      <AppButton btnType="primary">Submit</AppButton>      
+    </div>
+  </form>
+</template>
