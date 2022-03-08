7a43a68b1b1f0ea87c1f245eda7ad108412e0dab
Anthony <ach0074@gmail.com>
2022-03-08 11:31:59 -0500

    Created new components for BookReview

--- src/components/BookReview/BookReview.vue
+++ src/components/BookReview/BookReview.vue
@@ -2,6 +2,7 @@
 import { ref, inject, computed } from 'vue'
 import { bookReviewsList } from '../../store/bookReviewsList.js'
 import ReviewForm from '../ReviewForm/ReviewForm.vue'
+import UserReview from '../UserReview/UserReview.vue'
 
 const displayForm = ref(false)
 const reviewStore = bookReviewsList()
@@ -18,28 +19,14 @@ const currentBookReviews = computed(() => {
 
 <template>
   <div class="mt-4">
-
-    <!-- placeholder. convert to separate component when layout is done -->
     <div v-if="currentBookReviews.length > 0">
-      <div 
-        v-for="item in currentBookReviews" :key="item.reviewId"
-        class="border p-3"
-      >
-        <p v-if="item.isRecommended" class="italic">The reviewer recommends this book.</p>
-        <p v-else class="italic">The reviewer does not recommend this book.</p>
-        <p>{{ item.userReview }}</p>
-        <div class="border-t flex justify-between pt-4">
-          <p>{{ item.helpfulCount }} users found this review helpful</p>
-          <div class="flex">
-            <p>Was this review helpful?</p>
-            <span class="text-sm text-neutral-700">Yes</span>
-            <span class="text-sm text-neutral-700">No</span>             
-          </div>
-        </div>
-      </div>
+      <UserReview
+        v-for="review in currentBookReviews" :key="review.reviewId"
+        :isRecommended="review.isRecommended"
+        :userReview="review.userReview"
+        :helpfulCount="review.helpfulCount"
+      ></UserReview>
     </div>
-
-
     <div v-else>
       <div class="min-h-max py-4 bg-blue-100 rounded-md flex flex-col items-center justify-center">
         <p class="text-center text-base">There are currently no reviews available for this book.</p>
@@ -50,10 +37,7 @@ const currentBookReviews = computed(() => {
           >write</span> a review!
         </p>
       </div>
-      <div 
-        v-if="displayForm"
-        class="mt-4"
-      >
+      <div v-if="displayForm" class="mt-4">
         <ReviewForm @set-form-status="toggleForm" />
       </div>
     </div>
---
+++ src/components/UserReview/UserReview.vue
@@ -0,0 +1,28 @@
+<script setup>
+import ThumbsUp from '../ThumbsUp/ThumbsUp.vue'
+import ThumbsDown from '../ThumbsDown/ThumbsDown.vue'
+
+const props = defineProps({
+  isRecommended: { type: Boolean, required: true },
+  userReview: { type: String, required: true },
+  helpfulCount: { type: Number, required: true, default: 0 }
+})
+</script>
+
+<template>
+  <div class="border p-3">
+    <h3 v-if="props.isRecommended" class="font-semibold text-neutral-600 italic">The reviewer recommends this book.</h3>
+    <p v-else class="font-semibold text-neutral-600 italic">The reviewer does not recommend this book.</p>
+    <div class="mt-4 mb-6">
+      <p>{{ props.userReview }}</p>
+    </div>
+    <div class="border-t flex justify-between pt-4">
+      <p>{{ props.helpfulCount }} users found this review helpful</p>
+      <div class="flex">
+        <p>Was this review helpful?</p>
+        <ThumbsUp size="sm" />
+        <ThumbsDown size="sm" />
+      </div>
+    </div>
+  </div>
+</template>
