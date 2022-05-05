341262b5fe1a74fb77e0e10a33532432c8ab79bd
Anthony <ach0074@gmail.com>
2022-05-05 14:01:13 -0400

    Increased font size of heading

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,7 +7,7 @@ const store = bookCollections()
 <template>
   <div class="self-center flex">
     <div class="flex-1">
-      <h2 class="text-3xl text-neutral-700 capitalize">You have read <span class="text-violet-500">{{ store.getCollections.length }}</span> books</h2>
+      <h2 class="text-4xl text-neutral-700 capitalize">You have read <span class="text-violet-500">{{ store.getCollections.length }}</span> books</h2>
       <router-link to="/find-books" class="text-lg leading-10 text-violet-500 hover:text-violet-700">Find More Books to Read</router-link>
     </div>
     <div class="flex-1">
