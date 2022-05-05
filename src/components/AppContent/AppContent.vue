3bc0e3f118db6463c478601deaf9ea2e1bcf3d50
Anthony <ach0074@gmail.com>
2022-05-05 13:13:48 -0400

    Resolved console warning involving router-view and Transition

--- src/components/AppContent/AppContent.vue
+++ src/components/AppContent/AppContent.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="flex-1 flex min-h-max ml-4 p-4">
-    <Transition name="vert-slide">
-      <router-view></router-view>      
-    </Transition>
+    <router-view v-slot="{ Component }">
+      <Transition name="vert-slide" mode="out-in">
+        <component :is="Component" />
+      </Transition>
+    </router-view>
   </div>
 </template>
 
 <style scoped>
 .vert-slide-enter-active,
 .vert-slide-leave-active {
-  @apply transition-all translate-y-3 opacity-100;
+  @apply transition-transform translate-y-3 opacity-100;
 }
 
 .vert-slide-enter-from,
