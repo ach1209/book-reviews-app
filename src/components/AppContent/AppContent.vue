d8a73d0e639c08e6653d2593e1cb58f65f2c0363
Anthony <ach0074@gmail.com>
2022-05-05 11:33:05 -0400

    Added vertical slide transition to AppContent component

--- src/components/AppContent/AppContent.vue
+++ src/components/AppContent/AppContent.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="flex-1 flex min-h-max ml-4 p-4">
-    <router-view></router-view>
+    <Transition name="vert-slide">
+      <router-view></router-view>      
+    </Transition>
   </div>
 </template>
+
+<style scoped>
+.vert-slide-enter-active,
+.vert-slide-leave-active {
+  @apply transition-all translate-y-3 opacity-100;
+}
+
+.vert-slide-enter-from,
+.vert-slide-leave-to {
+  @apply translate-y-0 opacity-0;
+}
+</style>
